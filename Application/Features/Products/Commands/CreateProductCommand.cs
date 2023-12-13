ea48978f76babb3b0af7406440fd2ee95cd2b25b
mardibak <mardibaki@gmail.com>
2023-12-13 22:20:58 +0330

    CreateProductCommandHandler implemented

---
+++ Application/Features/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Products;
+using MediatR;
+
+namespace Application.Features.Products.Commands;
+public class CreateProductCommand : IRequest<int>
+{
+    public string Name { get; set; }
+    public string ManufacturePhone { get; set; }
+    public string ManufactureEmail { get; set; }
+};
+
+//public record CreateProductCommand(Product product) : IRequest;
+
---
+++ Application/Features/Products/CommandsHandler/CreateProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.Products.Commands;
+using Application.Interface;
+using Domain.Products;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Products.CommandsHandler
+{
+    public sealed class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+    {
+        private readonly IProductDbContext _productDbContext;
+
+        public CreateProductCommandHandler(IProductDbContext productDbContext)
+        {
+            _productDbContext = productDbContext;
+        }
+
+        public async Task<int> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product(
+                request.Name,
+                request.ManufacturePhone,
+                request.ManufactureEmail);
+
+            await _productDbContext.Products.AddAsync(product);
+            await _productDbContext.SaveChangeAsync();
+
+            return product.Id;
+        }
+    }
+}

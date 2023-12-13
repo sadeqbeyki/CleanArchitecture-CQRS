537948598f960409bc50463b46725c7c289e6905
mardibak <mardibaki@gmail.com>
2023-12-13 21:12:00 +0330

    Add project files.

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Common;
+
+public class BaseEntity
+{
+    public int Id { get; private set; }
+    public DateTime ProduceDate { get; private set; }
+
+    public BaseEntity()
+    {
+        ProduceDate = DateTime.Now;
+    }
+}
---
+++ Domain/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+
+namespace Domain.Products;
+
+public class Product : BaseEntity
+{
+    public string Name { get; private set; }
+    public string ManufacturePhone { get; private set; }
+    public string ManufactureEmail { get; private set; }
+    public bool IsAvailable { get; private set; }
+
+
+    public Product(string name, string manufacturePhone, string manufactureEmail)
+    {
+        Name = name;
+        ManufacturePhone = manufacturePhone;
+        ManufactureEmail = manufactureEmail;
+    }
+}

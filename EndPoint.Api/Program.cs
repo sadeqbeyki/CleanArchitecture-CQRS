020f6bbdf0c07334350167fd7326d25975e89827
mardibak <mardibaki@gmail.com>
2023-12-15 00:36:36 +0330

    jwt implemented-completed / dependencyInjection reConfigured

--- EndPoint.Api/Program.cs
+++ EndPoint.Api/Program.cs
@@ -1,35 +1,108 @@
 using Application;
 using Identity.Application;
-using Identity.Application.Features.User.QueryHandlers;
+using Identity.Application.Interface;
 using Identity.Infrastructure;
-using Identity.Persistance;
-using Identity.Persistance.Identity;
+using Identity.Services;
 using Infrastructure;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using Persistance;
-using System.Reflection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-//ServiceExtentions.AddInfrastructure(builder.Configuration);
-//builder.Services.AddMediatR(typeof(CreateUserCommandHandler).GetTypeInfo().Assembly);
+//_________ Authentication
+#region Authentication
+var _key = builder.Configuration["Jwt:Key"];
+var _issuer = builder.Configuration["Jwt:Issuer"];
+var _audience = builder.Configuration["Jwt:Audience"];
+var _expirtyMinutes = builder.Configuration["Jwt:ExpiryMinutes"];
+#endregion
+
+//_____________ Token Configuration
+#region TokenConfiguration
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = _audience,
+        ValidIssuer = _issuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key)),
+        ClockSkew = TimeSpan.FromMinutes(Convert.ToDouble(_expirtyMinutes))
+
+    };
+});
+#endregion
+
+//_______________ Add DependencyInjection
+#region DependencyInjection
+
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddIdentityApplication();
 builder.Services.AddIdentityInfrastructure(builder.Configuration);
+#endregion
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<AppIdentityDbContext>()
-    .AddDefaultTokenProviders();
+//__________________ Token Generator / Add Cors
+#region TokenGenerator/CorsPolicy
+builder.Services.AddSingleton<ITokenGenerator>(new TokenGenerator(_key, _issuer, _audience, _expirtyMinutes));
+
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("CorsPolicy", options => options.AllowAnyOrigin()
+    .AllowAnyMethod().AllowAnyHeader());
+});
+#endregion
+
+builder.Services.AddEndpointsApiExplorer();
+
+//_____________________ Enable Authorization in swagger
+#region EnableAuthorizationInSwagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer {token}\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+                {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+
+                    }
+                });
+
+});
+#endregion
 
 var app = builder.Build();
 
@@ -45,6 +118,11 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
+//_____________________Maintain middleware product
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+//__
 app.UseAuthorization();
 
 app.MapControllers();

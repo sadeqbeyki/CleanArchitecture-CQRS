41c801ecddcfbaaa0b46d93036e95af8646f598e
sadeq.beyki <mardibaki@gmail.com>
2024-01-28 15:01:07 +0330

    blog domain project added (with entities)

---
+++ Blog.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Domain.Entities.Base;
+
+public class BaseEntity<T>
+{
+    public T Id { get; private set; } = default!;
+    public DateTime CreatedAt { get; private set; }
+
+    public BaseEntity()
+    {
+        CreatedAt = DateTime.Now;
+    }
+}

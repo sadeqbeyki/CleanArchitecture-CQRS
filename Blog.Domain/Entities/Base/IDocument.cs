0b9f24cf5ccc0b218c54508f21999a2f61e0b59d
sadeq.beyki <mardibaki@gmail.com>
2024-02-01 15:51:37 +0330

    add document and replace with base entity

---
+++ Blog.Domain/Entities/Base/IDocument.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Blog.Domain.Entities.Base;
+
+public interface IDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    ObjectId Id { get; set; }
+
+    DateTime CreatedAt { get; }
+}
+
+public abstract class Document : IDocument
+{
+    public ObjectId Id { get; set; }
+
+    public DateTime CreatedAt => Id.CreationTime;
+}
